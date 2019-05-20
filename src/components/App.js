c9dbadede3d377ca844f6e36bddb9b1a5d216c9c
Parker Helland <hellandp@gmail.com>
2019-05-20 14:18:12 -0700

    initial commit, including rending

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Display  from '../containers/Display';
+
+export default function App() {
+  return <Display />;
+}
---
+++ src/containers/Display.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from 'react-router-dom';
+
+export default class Display extends PureComponent {
+  // state = {
+
+  // }
+
+  render() {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
+}
