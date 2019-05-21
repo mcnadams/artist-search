e8bc4b6b6ae6c58b2d01c806bde83b52123a02f1
Parker Helland <hellandp@gmail.com>
2019-05-21 11:37:20 -0700

    base commit for Song functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import {
 } from 'react-router-dom';
 import Search from '../containers/artist/Search';
 import AlbumDisplay from '../containers/album/albumDisplay';
+import SongDisplay from '../containers/song/songDisplay';
 
 export default function App() {
   return (
     <Router>
       <Route exact path="/" component={Search} />
       <Route path="/artist/:id" component={AlbumDisplay} />
+      <Route path="/album/:id" component={SongDisplay} />
     </Router>
   );
 }
